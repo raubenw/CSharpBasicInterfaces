d28d8342fae82b92818f98a20ff0f2695574eb60
raubenw <91202958+raubenw@users.noreply.github.com>
2022-03-03 03:43:51 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpBasicInterfaces {
+
+    interface IStorable {
+        void Save();
+        void Load();
+        bool NeedsSave {get; set;} 
+    }
+
+    class Document : IStorable {
+
+        private string name;
+
+        public Document(string s) {
+            name = s;
+            Console.WriteLine("Document created with name '{0}'", name);
+        }
+
+        public void Save() {
+            Console.WriteLine("Saving document)");
+        }
+
+        public void Load() {
+            Console.WriteLine("Loading document");
+        }
+
+        public bool NeedsSave {
+            get; set;
+        }
+    } // End class Document
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            Document d = new Document("Test Document");
+            d.Load();
+            d.Save();
+            d.NeedsSave = false;
+
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+
+}
